1c0e5e2e4e145bb5e938dfb248dd5d8c2e918bbf
hungnt166-boop <hungnt166@gmail.com>
2025-11-10 02:01:12 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tn-hnkh-2025-korea-v1';
+const CACHE = 'tn-hnkh-2025-korea-v3';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
@@ -9,15 +9,14 @@ self.addEventListener('activate', e=>{
   self.clients.claim();
 });
 self.addEventListener('fetch', e=>{
-  const req = e.request;
-  if(req.method !== 'GET') return;
+  if(e.request.method!=='GET') return;
   e.respondWith((async()=>{
     const cache = await caches.open(CACHE);
-    const hit = await cache.match(req);
-    if(hit) { return hit; }
+    const hit = await cache.match(e.request);
+    if(hit) return hit;
     try{
-      const net = await fetch(req);
-      cache.put(req, net.clone());
+      const net = await fetch(e.request);
+      cache.put(e.request, net.clone());
       return net;
     }catch(err){
       return cache.match('./index.html');
